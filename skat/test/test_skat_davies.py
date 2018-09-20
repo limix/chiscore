9771c86e1744edf07154b0d0dd0ecb0a6d0308af
Danilo Horta <danilo.horta@gmail.com>
2018-09-20 10:29:26 +0100

    skat test working too

--- skat/test/test_skat_davies.py
+++ skat/test/test_skat_davies.py
@@ -1,10 +1,4 @@
-# def test_SKAT_Optimal_PValue_Davies():
-#     data = np.load("SKAT_Optimal_PValue_Davies.npz")
-#     pval = SKAT_Optimal_PValue_Davies(*data["args"])
-#     assert abs(pval - data["pval"]) < 1e-9
-
-
-from skat import davies_pvalue, data_file
+from skat import skat_davies_pvalue, data_file
 from numpy import load
 from numpy.testing import assert_allclose
 
@@ -13,5 +7,5 @@ def test_skat_davies_pvalue():
     with data_file("skat_davies_pvalue.npz") as filepath:
         data = load(filepath)
 
-    pval = davies_pvalue(*data["args"])["p_value"]
+    pval = skat_davies_pvalue(*data["args"])
     assert_allclose(pval, data["pval"])
