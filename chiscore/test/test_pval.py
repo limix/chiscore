c664a7017153f142d1b1536c2603d9ce985e3062
Danilo Horta <danilo.horta@pm.me>
2020-07-10 16:16:07 +0100

    large lambdas

--- chiscore/test/test_pval.py
+++ chiscore/test/test_pval.py
@@ -5,7 +5,7 @@ from scipy.stats import ncx2
 from chiscore import davies_pvalue, liu_sf
 
 
-def test_pval_calibration():
+def test_pval_calibration1():
     dof = [1, 1, 1]
     w = [0.5, 0.4, 0.1]
     loc = [0.0, 0.0, 0.0]
@@ -18,6 +18,18 @@ def test_pval_calibration():
 
     pvals = liu_sf(samples, w, dof, loc, kurtosis=True)[0]
     assert_allclose(median(pvals), 0.5, rtol=1e-2)
+
+    values = [davies_pvalue(s, diag(w)) for s in samples]
+    assert_allclose(median(values), 0.5, rtol=1e-2)
+
+
+def test_pval_calibration2():
+    dof = [1, 1, 1]
+    w = [128372.0, 23720.11, 528372.0]
+    loc = [0.0, 0.0, 0.0]
+
+    random.seed(2)
+    samples = _sample(w, dof, loc, n=5000)
 
     values = [davies_pvalue(s, diag(w)) for s in samples]
     assert_allclose(median(values), 0.5, rtol=1e-2)
